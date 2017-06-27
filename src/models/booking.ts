ebfbe3c8149ceaeb01595bfe75d0a9a0d8079bdb
Hamurabi Araújo <hamurabi@Danielles-MacBook-Air.local>
2017-06-27 02:20:06 -0300

    Booking and rollerblade models added

---
+++ src/models/booking.ts
@@ -0,0 +1,11 @@
+import { Rollerblade } from "./rollerblade";
+
+export class Booking {
+  id: number;
+  startDateTime: Date;
+  endDateTime: Date;
+  rollerblade: Rollerblade;
+
+  constructor() { }
+
+}
---
+++ src/models/rollerblade.ts
@@ -0,0 +1,10 @@
+export class Rollerblade {
+  id: number;
+  cod: string;
+  color: string;
+  image: string;
+  brand: string;
+  size: number;
+
+  constructor() { }
+}
