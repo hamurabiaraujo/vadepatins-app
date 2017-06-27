588aa0ef5c2f1001a05cd73954b2f7e2fe53186b
Hamurabi Araújo <hamurabi@Danielles-MacBook-Air.local>
2017-06-26 22:06:23 -0300

    Messages and placeholders updated

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -3,6 +3,7 @@ import { NavController, ToastController } from 'ionic-angular';
 
 import { MainPage } from '../../pages/pages';
 import { User } from '../../providers/user';
+import { LoginPage } from "../login/login";
 
 import { TranslateService } from '@ngx-translate/core';
 
@@ -16,21 +17,21 @@ export class SignupPage {
   // If you're using the username field with or without email, make
   // sure to add it to the type
   account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
+    name: '',
+    email: '',
+    password: ''
   };
 
   // Our translated text strings
-  private signupErrorString: string;
+  private signupConfirmationString: string;
 
   constructor(public navCtrl: NavController,
     public user: User,
     public toastCtrl: ToastController,
     public translateService: TranslateService) {
 
-    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
-      this.signupErrorString = value;
+    this.translateService.get('SIGNUP_CONFIRMATION').subscribe((value) => {
+      this.signupConfirmationString = value;
     })
   }
 
@@ -44,7 +45,7 @@ export class SignupPage {
 
       // Unable to sign up
       let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
+        message: this.signupConfirmationString,
         duration: 3000,
         position: 'top'
       });
